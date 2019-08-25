e84b752b1dc6f9c467591b000259d37818b8628f
Nate Ng <nateng@Nates-MacBook-Pro.local>
2019-08-25 03:04:16 -0700

    Profile Page JSON parsed completed

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -2,22 +2,53 @@ import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 
 class profile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: [],};
+    }
+
+    callAPI() {
+        fetch('/user')
+            .then(res => res.json())
+            .then((data) => this.setState({apiResponse: data}))
+            .catch(err => err);
+    }
+
+    componentDidMount() {
+        // fetch('/user')
+        //     .then(data => data.json())
+        //     .then(userInfo => this.setState({apiResponse: userInfo}))
+        this.callAPI();
+    }
+
     render() {
         return (
         <div style={{width: '100%', margin: 'auto'}}>
             <Grid className = 'landing-grid'>
                 <Cell col={12}>
+                    {this.state.apiResponse.map(info => (
                     <img 
-                        src='https://naibac.com/includes/agent_pictures/3652.jpg'
+                        src={info.picture.large}
                         alt='avatar'
                         className='avatar-img'
                     />
+                    ))}
                     <div className='banner-text'>
-                        <h2 id='title'>First Name, Last Name</h2>
-
+                        <div className = 'user-info'>
+                            {this.state.apiResponse.map(info => (
+                                <h2>{info.name.first} {info.name.last}</h2>
+                            ))}
+                        </div>
                         <hr />
-                        <p>Email: email@domainsample.com</p>
-                        <p>Number: 05-912-12382</p>
+                        <div className = 'user-contacts'>
+                            {this.state.apiResponse.map(info => (
+                                <div>
+                                    <p className='contact-email'><strong>Email:</strong> {info.email}</p>
+                                    <p className='contact-home'><strong>Home:</strong> {info.phone}</p>
+                                    <p className='contact-cell'><strong>Cell:</strong> {info.cell}</p>
+                                </div>
+                            ))}
+                        </div>
                         <div className="social-links">
 
                             {/* Email button */}
@@ -29,9 +60,9 @@ class profile extends Component {
                             <a href='http://google.com' rel='noopener noreferrer' target='_blank'>
                                 <i className='fa fa-linkedin-square' aria-hidden='true' />
                             </a>
-
                         </div>
                     </div>
+
                 </Cell>
             </Grid>
         </div>
