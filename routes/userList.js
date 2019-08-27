03c32216f6579cf701f315f8112b34221db62390
Nate Ng <nateng@Nates-MacBook-Pro.local>
2019-08-26 22:08:29 -0700

    Repair routes

--- routes/userList.js
+++ routes/userList.js
@@ -2,7 +2,9 @@ const connection = require('../models/db');
 
 module.exports = app => {
     app.get('/users', (req, res, next) => {
-        connection.query('SELECT * FROM user_profiles;', (error, results) => {
+        connection.query('SELECT uuid,gender,firstname,\
+        lastName,title,email,dob,picture FROM user_profiles\
+        GROUP BY 1,2,3,4,5,6,7,8;', (error, results) => {
             if(error) {
                 next(error);
             }
